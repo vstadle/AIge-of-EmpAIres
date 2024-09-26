33ef811fda6f8aec3d5f28fdabec0df29b0a1634
Valentin STADLER <valentin.stadler@universite-paris-saclay.fr>
2024-09-26 18:49:11 +0200

    Ajout des affiches pour la classe Keek

--- src/model/Keep.py
+++ src/model/Keep.py
@@ -7,5 +7,19 @@ class Keep(Buildings):
         self.costG = 125
         self.attack = 5
         self.range = 8
+
+    def __repr__(self):
+        return "Keep(%r, %r, %r, %r, %r, %r, %r)" % (self.costW, self.bTime, self.hp, self.sizeMap, self.costG, self.attack, self.range)
+    
+    def print_Keep(self):
+        print("Keep")
+        print("Cost Wood: ", self.costW)
+        print("Build Time: ", self.bTime)
+        print("Health: ", self.hp)
+        print("Size: ", self.sizeMap)
+        print("Cost Gold: ", self.costG)
+        print("Attack: ", self.attack)
+        print("Range: ", self.range)
+
         
     
