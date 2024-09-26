135a1979aea61061889397e40df24bdb3ef5c670
Valentin STADLER <valentin.stadler@universite-paris-saclay.fr>
2024-09-26 18:49:57 +0200

    Déplacement des classes dans le dossier src

--- model/Horseman.py
+++
@@ -1,6 +0,0 @@
-from units import Units
-
-class Horseman(Units):
-
-    def__init__(self):
-        super().__init__(80,40,20,4,1.2,1)
--- model/Villager.py
+++
@@ -1,8 +0,0 @@
-from units import Units
-
-class Villager(Units):
-
-    def __init__(self):
-        super().__init__(50, 25, 25, 2, 1, 0.8, 1)
-        self.collectRessources = 25 #min
-        self.carryingCapacity = 20 #min
