e39b1e457cfa636d15524285fc77f0700d969398
nmassot-web <nora.massot@insa-cvl.fr>
2024-10-11 15:25:05 +0200

    Player.py

--- src/model/Player.py
+++ src/model/Player.py
@@ -1,13 +1,37 @@
 
 class Player():
 
-    def __init__(self, name):
+    def __init__(self, name, f, w, g):
         self.name = name
         self.units = []
         self.buildings = []
+        self.food = f
+        self.wood = w
+        self.gold = g
 
     def addUnit(self, unit):
         self.units.append(unit)
 
     def addBuilding(self, building):
         self.buildings.append(building)
+        
+    def addFood(self, Food):
+        self.food += Food
+    
+    def addWood(self, Wood):    
+        self.wood += Wood
+        
+    def addGold(self, Gold):
+        self.gold += Gold
+        
+    def removeFood(self, Food):
+        self.food -= Food
+    
+    def removeWood(self, Wood):    
+        self.wood -= Wood
+        
+    def removeGold(self, Gold):
+        self.gold -= Gold
+    
+    def countUnits(self):
+        return len(self.units)
