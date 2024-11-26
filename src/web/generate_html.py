f076442584adeccb8b140f41343dda08fdd9dd3b
Valentin STADLER <valentin.stadler@universite-paris-saclay.fr>
2024-11-26 19:16:19 +0100

    [STADLER] Ajout de l'affichage pour les différentes types de troupes sur la page web

--- src/web/generate_html.py
+++ src/web/generate_html.py
@@ -1,11 +1,23 @@
-import model.Game as Game
+from model.Archer import Archer
+from model.Villager import Villager
+from model.Horseman import Horseman
+from model.Swordsman import Swordsman
+from model.Farm import Farm
+from model.Camp import Camp
+from model.ArcheryRange import ArcheryRange
+from model.Barracks import Barracks
+from model.House import House
+from model.Keep import Keep
+from model.Stable import Stable
+from model.TownCenter import TownCenter
 
 def generateHtml(lstPlayers):
     clear_html()
     print("Generating HTML")
-    print(lstPlayers)
     content = ""
     for player in lstPlayers:
+        units_type = calculate_unit(player.getPlayer())
+        buildings_type = calculate_building(player.getPlayer())
         content += f"""
         <h2>{player.getPlayer().name}</h2>
         <div class="ressources">
@@ -16,14 +28,24 @@ def generateHtml(lstPlayers):
         </div>
         <div class="units">
             <h3>Units</h3>
-            <p>Number of Units: {len(player.getPlayer().units)}</p>
+            <p>Number of Villager: {units_type[0]}</p>
+            <p>Number of Archer: {units_type[1]}</p>
+            <p>Number of Horseman: {units_type[2]}</p>
+            <p>Number of Swordsman: {units_type[3]}</p>
             <ul>
                 {''.join([f"<li>{unit}</li>" for unit in player.getPlayer().units])}
             </ul>
         </div>
         <div class="buildings">
             <h3>Buildings</h3>
-            <p>Number of Buildings: {len(player.getPlayer().buildings)}</p>
+            <p>Number of TownCenter: {buildings_type[0]}</p>
+            <p>Number of Farm: {buildings_type[1]}</p>
+            <p>Number of Camp: {buildings_type[2]}</p>
+            <p>Number of Archery Range: {buildings_type[3]}</p>
+            <p>Number of Barracks: {buildings_type[4]}</p>
+            <p>Number of House: {buildings_type[5]}</p>
+            <p>Number of Keep: {buildings_type[6]}</p>
+            <p>Number of Stable: {buildings_type[7]}</p>
             <ul>
                 {''.join([f"<li>{building}</li>" for building in player.getPlayer().buildings])}
             </ul>
@@ -31,6 +53,55 @@ def generateHtml(lstPlayers):
         """
     
     insert_html(content)
+    print("HTML generated")
+
+def calculate_unit(player):
+    cptVillager = 0
+    cptArcher = 0
+    cptHorseman = 0
+    cptSwordsman = 0
+
+    for unit in player.units:
+        if type(unit) == Villager:
+            cptVillager += 1
+        elif type(unit) == Archer:
+            cptArcher += 1
+        elif type(unit) == Horseman:
+            cptHorseman += 1
+        elif type(unit) == Swordsman:
+            cptSwordsman += 1
+    units = [cptVillager, cptArcher, cptHorseman, cptSwordsman]
+    return units
+
+def calculate_building(player):
+    cptTownCenter = 0
+    cptFarm = 0
+    cptCamp = 0
+    cptArcheryRange = 0
+    cptBarracks = 0
+    cptHouse = 0
+    cptKeep = 0
+    cptStable = 0
+
+    for building in player.buildings:
+        if type(building) == TownCenter:
+            cptTownCenter += 1
+        elif type(building) == Farm:
+            cptFarm += 1
+        elif type(building) == Camp:
+            cptCamp += 1
+        elif type(building) == ArcheryRange:
+            cptArcheryRange += 1
+        elif type(building) == Barracks:
+            cptBarracks += 1
+        elif type(building) == House:
+            cptHouse += 1
+        elif type(building) == Keep:
+            cptKeep += 1
+        elif type(building) == Stable:
+            cptStable += 1
+    buildings = [cptTownCenter, cptFarm, cptCamp, cptArcheryRange, cptBarracks, cptHouse, cptKeep, cptStable]
+    return buildings
 
 def insert_html(content):
     # Assure-toi que tu as bien le chemin complet vers ton fichier HTML
