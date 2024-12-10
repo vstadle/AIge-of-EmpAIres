c12b5fab8dd31c70ca6ee1cf2f3250e306edc513
Ilovetechnologyman <teuglife875@gmail.com>
2024-12-10 14:32:29 +0100

    tampon

--- src/controller/ControllerMap.py
+++ src/controller/ControllerMap.py
@@ -3,8 +3,6 @@ import sys
 import random
 import math
 import time
-import webbrowser
-import os
 
 from view.ViewMap import ViewMap
 from model.Map import Map, MapType
@@ -14,31 +12,20 @@ from model.Villager import Villager
 from model.Archer import Archer
 from model.Horseman import Horseman
 from model.Swordsman import Swordsman
-from web.generate_html import generateHtml
+
+
 
 class ControllerMap():
-    def __init__(self, uiHandler):
+    def __init__(self):
         pygame.init()
         self.map = Map(MapType.GENEROUS_RESOURCES)
         self.vMap = ViewMap(self.map, self)  # Passer la référence de la carte
         self.pos_x = 0
         self.pos_y = 0
+        self.zoom_level = 1.0
         self.training_queue = []
-        self.lstPlayers = []
-        self.uiHandler = uiHandler
-        self.paused = False
-        self.tab_pressed = False
-
-    def reset(self, map):
-        self.map = map
-        self.vMap = ViewMap(self.map, self)
-        self.pos_x = 0
-        self.pos_y = 0
-        self.training_queue = []
-        self.lstPlayers = []
-
-    def setLstPlayers(self, lstPlayers):
-        self.lstPlayers = lstPlayers
+        self.display_mode = "2D"  # Mode d'affichage initial
+        self.zoom_level = 1.0
 
     def placementTownCenter(self, nbPlayer, lstPlayers):
         position = []
@@ -54,9 +41,12 @@ class ControllerMap():
 
         cpt = 0
         for i in position:
-            self.map.addBuilding(TownCenter(), int(i[0]), int(i[1]), lstPlayers[cpt].getPlayer())
+            self.map.addBuilding(TownCenter(), int(i[0]), int(i[1]))
             lstPlayers[cpt].addBuildingInitialize(TownCenter(), int(i[0]), int(i[1]))
             cpt += 1
+
+    def addBuilding(self, building, x, y):
+        self.map.addBuilding(building, x, y)
 
     def genRessources(self, map_type):
         if map_type == MapType.GENEROUS_RESOURCES:
@@ -64,61 +54,89 @@ class ControllerMap():
         elif map_type == MapType.CENTER_RESOURCES:
             self.map.generateCenterResources()
         self.map.generateForest()
+
+    def addUnits(self, unit, player, building):
+        start_time = time.time()
+        self.training_queue.append({"unit": unit, "player": player, "start_time": start_time, "building": building})
+
+    def update_training_units(self):
+        current_time = time.time()
+        for item in self.training_queue[:]:  # Itérer sur une copie de la liste
+            unit = item["unit"]
+            player = item["player"]
+            start_time = item["start_time"]
+            building = item["building"]
+
+            if current_time - start_time >= unit.trainingTime:
+                x = building.getX()
+                y = building.getY()
+                building_width, building_height = building.getSizeMap(), building.getSizeMap()
+
+                placed = False
+                layer = 1  # On commence par la couche directement autour du bâtiment
+
+                while not placed:
+                    # Parcours des cases autour de la couche actuelle
+                    for dx in range(-layer, layer + 1):
+                        for dy in range(-layer, layer + 1):
+                            # Vérifier uniquement les positions aux bords de la couche
+                            if abs(dx) == layer or abs(dy) == layer:
+                                nx, ny = x + dx, y + dy
+                                if self.map.is_free(nx, ny):
+                                    self.map.addUnits(unit, nx, ny)
+                                    player.addUnit(unit)
+                                    placed = True
+                                    break
+                        if placed:
+                            break
+                    if not placed:
+                        
+                        layer += 1
+
+                
+                self.training_queue.remove(item)
     
     def run(self):
         clock = pygame.time.Clock()
         while True:
             for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_v:
+                        self.display_mode = "2.5D" if self.display_mode == "2D" else "2D"
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_TAB and not self.tab_pressed:
-                        self.tab_pressed = True
-                        self.toggle_pause()
-        
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_TAB:
-                        self.tab_pressed = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 4:  
+                        self.zoom_level = round(min(1.4, self.zoom_level + 0.1), 1)
+                    elif event.button == 5:  
+                        self.zoom_level = round(max(0.5, self.zoom_level - 0.1), 1)
+                        if self.zoom_level < 0.1:
+                            self.zoom_level = 0.1
 
             keys = pygame.key.get_pressed()
+            if keys[pygame.K_z]: self.pos_y -= 1
+            if keys[pygame.K_s]: self.pos_y += 1
+            if keys[pygame.K_q]: self.pos_x -= 1
+            if keys[pygame.K_d]: self.pos_x += 1
+            if keys[pygame.K_p]: sys.exit()
+            
+            max_x = max(0, len(self.map.map[0]) - (self.vMap.GRID_WIDTH / self.zoom_level))
+            max_y = max(0, len(self.map.map) - (self.vMap.GRID_HEIGHT / self.zoom_level))
 
-            if not self.paused:
-                if keys[pygame.K_z]: self.pos_y -= 1
-                if keys[pygame.K_s]: self.pos_y += 1
-                if keys[pygame.K_q]: self.pos_x -= 1
-                if keys[pygame.K_d]: self.pos_x += 1
-                if keys[pygame.K_ESCAPE]: webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                if keys[pygame.K_p]: 
-                    self.uiHandler.saveGame()
-                    sys.exit()
+            self.pos_x = max(0, min(self.pos_x, max_x))
+            self.pos_y = max(0, min(self.pos_y, max_y))
 
-                max_x = len(self.map.map[0]) - self.vMap.GRID_WIDTH
-                max_y = len(self.map.map) - self.vMap.GRID_HEIGHT
-                self.pos_x = max(0, min(self.pos_x, max_x))
-                self.pos_y = max(0, min(self.pos_y, max_y))
-
+            if self.display_mode == "2D":
                 self.vMap.draw_map(self.vMap.screen, self.pos_x, self.pos_y)
-                pygame.display.flip()
+            else:
+                self.vMap.draw_map_2_5D(self.vMap.screen, self.pos_x, self.pos_y, self.zoom_level)
 
-                #self.update_training_units()  # Mettre à jour les unités en entraînement
+            pygame.display.flip()
 
-                for player in self.lstPlayers:
-                    player.update_training()
-                    player.update_building()
+            self.update_training_units()  
 
-                clock.tick(30)
-
-    def toggle_pause(self):
-        self.paused = not self.paused
-        if self.paused:
-            print("Paused")
-            generateHtml(self.lstPlayers)
-            current_path = "file://" + os.getcwd() + "/web/index.html"
-            webbrowser.open(current_path)
-        else:
-            print("Unpaused")
+            clock.tick(30)
 
     def getMap(self):
         return self.map
