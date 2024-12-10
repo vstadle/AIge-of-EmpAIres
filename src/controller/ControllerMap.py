c136a3913feae05e41e6a43356fa6f53dd5642cc
vstadle <valentin.stadler@icloud.com>
2024-12-10 14:58:46 +0100

    [STADLER] Nettoyage du code mort

--- src/controller/ControllerMap.py
+++ src/controller/ControllerMap.py
@@ -6,7 +6,6 @@ import time
 import webbrowser
 import os
 
-from view.ViewMap import ViewMap
 from model.Map import Map, MapType
 from model.Player import Player
 from model.TownCenter import TownCenter
@@ -17,15 +16,13 @@ from model.Swordsman import Swordsman
 from web.generate_html import generateHtml
 
 class ControllerMap():
-    def __init__(self, uiHandler):
+    def __init__(self):
         pygame.init()
         self.map = Map(MapType.GENEROUS_RESOURCES)
-        self.vMap = ViewMap(self.map, self)  # Passer la référence de la carte
         self.pos_x = 0
         self.pos_y = 0
         self.training_queue = []
         self.lstPlayers = []
-        self.uiHandler = uiHandler
         self.paused = False
         self.tab_pressed = False
 
@@ -64,61 +61,6 @@ class ControllerMap():
         elif map_type == MapType.CENTER_RESOURCES:
             self.map.generateCenterResources()
         self.map.generateForest()
-    
-    def run(self):
-        clock = pygame.time.Clock()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_TAB and not self.tab_pressed:
-                        self.tab_pressed = True
-                        self.toggle_pause()
-        
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_TAB:
-                        self.tab_pressed = False
-
-            keys = pygame.key.get_pressed()
-
-            if not self.paused:
-                if keys[pygame.K_z]: self.pos_y -= 1
-                if keys[pygame.K_s]: self.pos_y += 1
-                if keys[pygame.K_q]: self.pos_x -= 1
-                if keys[pygame.K_d]: self.pos_x += 1
-                if keys[pygame.K_ESCAPE]: webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                if keys[pygame.K_p]: 
-                    self.uiHandler.saveGame()
-                    sys.exit()
-
-                max_x = len(self.map.map[0]) - self.vMap.GRID_WIDTH
-                max_y = len(self.map.map) - self.vMap.GRID_HEIGHT
-                self.pos_x = max(0, min(self.pos_x, max_x))
-                self.pos_y = max(0, min(self.pos_y, max_y))
-
-                self.vMap.draw_map(self.vMap.screen, self.pos_x, self.pos_y)
-                pygame.display.flip()
-
-                #self.update_training_units()  # Mettre à jour les unités en entraînement
-
-                for player in self.lstPlayers:
-                    player.update_training()
-                    player.update_building()
-
-                clock.tick(30)
-
-    def toggle_pause(self):
-        self.paused = not self.paused
-        if self.paused:
-            print("Paused")
-            generateHtml(self.lstPlayers)
-            current_path = "file://" + os.getcwd() + "/web/index.html"
-            webbrowser.open(current_path)
-        else:
-            print("Unpaused")
 
     def getMap(self):
         return self.map
