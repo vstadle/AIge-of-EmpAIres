548119dc57f4f14c516b6a9fd99be2876518f5b4
StarBuckyyy <validzicgoran@gmail.com>
2025-01-25 02:46:58 +0100

    Map culling fini + fixssss

--- src/view/Camera.py
+++ src/view/Camera.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 
 class Camera:
     def __init__(self, width, height, grid_length_x, grid_length_y):
@@ -11,10 +11,12 @@ class Camera:
         self.grid_length_x = grid_length_x
         self.grid_length_y = grid_length_y
         
-        # Pre-calculate max scroll boundaries
-        self.max_scroll_x = grid_length_x * self.TILE_SIZE * 2 - width + 100000
-        self.max_scroll_y = grid_length_y * self.TILE_SIZE + self.TILE_SIZE + 15 - height + 100000
-
+        # Pré-calcul des limites isométriques
+        self.max_scroll_x = (grid_length_x * self.TILE_SIZE) - (width/2) + 1000
+        self.max_scroll_y = (grid_length_y * self.TILE_SIZE) - (height/2) + 1000
+        self.iso_offset_x = (abs(grid_length_x - grid_length_y) * self.TILE_SIZE) / 2
+        self.iso_offset_y = (grid_length_y - 0.5 * grid_length_x) * self.TILE_SIZE
+        
     def handle_input(self):
         keys = pygame.key.get_pressed()
         speed = self.speed_max if pygame.key.get_mods() & pygame.KMOD_SHIFT else self.speed
@@ -30,19 +32,28 @@ class Camera:
         self.clamp_scroll()
 
     def clamp_scroll(self):
-        # Using pre-calculated boundaries
-        self.scroll.x = max(-self.max_scroll_x, min(self.scroll.x,100000))
-        self.scroll.y = max(-self.max_scroll_y, min(self.scroll.y, 100000))
+        # Utilisation des limites de la carte
+        self.scroll.x = max(-self.max_scroll_x - self.iso_offset_x, min(self.scroll.x, self.max_scroll_x + self.iso_offset_x))
+        self.scroll.y = max(-self.max_scroll_y - self.iso_offset_y, min(self.scroll.y, self.max_scroll_y + self.iso_offset_y))
 
     def handle_minimap_navigation(self, mouse_pos, minimap_rect, grass_tiles_size):
         if not (pygame.mouse.get_pressed()[0] and minimap_rect.collidepoint(mouse_pos)):
             return
-        
+    
+        # Calculer la position relative du clic sur la minimap
         rel_x = (mouse_pos[0] - minimap_rect.x) / minimap_rect.width
         rel_y = (mouse_pos[1] - minimap_rect.y) / minimap_rect.height
-        
-        # Ajustement pour centrer horizontalement
-        self.scroll.x = -(rel_x * grass_tiles_size[0] - grass_tiles_size[0] / 2)
-        self.scroll.y = -(rel_y * grass_tiles_size[1])
-        
+
+        # Calculer l'offset nécessaire
+        viewport_width = self.width
+        viewport_height = self.height
+    
+        # Ajuster le calcul en x pour centrer correctement le viewport
+        # On ajoute un demi-décalage pour centrer la vue
+        target_scroll_x = (rel_x * (grass_tiles_size[0] - viewport_width)) - (grass_tiles_size[0] / 2)
+        target_scroll_y = (rel_y * (grass_tiles_size[1] - viewport_height))
+    
+        # Ajustement pour centrer la caméra par rapport au clic
+        self.scroll.x = -target_scroll_x + viewport_width / 2 - self.iso_offset_x
+        self.scroll.y = -target_scroll_y + viewport_height / 2
         self.clamp_scroll()
